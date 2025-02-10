eb5e543b24c12fc966b0009d95fdf4dfe824513a
Sam <samuelakisanmi7@gmail.com>
2025-02-10 02:51:00 +0000

    first

---
+++ S2 _CA1_Web API/Models/Employer.cs
@@ -0,0 +1,10 @@
+﻿namespace S2__CA1_Web_API.Models
+{
+    public class Employer
+    {
+        public Guid empId { get; set; }//-- Guid Globe unique identifier--//
+        public required string Title { get; set; } //-- input required --/
+        public required string Email { get; set; } //-- input required --/
+        public string? Phone { get; set; } //-- nullabe it can be left empty--//
+        public required string address { get; set;}
+}
---
+++ S2 _CA1_Web API/Models/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace S2__CA1_Web_API.Models.Entities
+{
+    public class Employee
+    {
+        public Guid empId { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+        public decimal Salary { get; set; }
+        
+
+    }
+}
